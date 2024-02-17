8cba9feb2abd2b72fbcaca15315c25fa1e8af2cc
Dilkrokodil <dil.zubajdullina@yandex.ru>
2024-02-17 14:16:47 +0300

    dz

--- dz/dz.py
+++ dz/dz.py
@@ -1,36 +1,13 @@
-d = {
-    "John": {
-        "N": 3056,
-        "S": 8463,
-        "E": 8441,
-        "W": 2694
-    },
-    "Tom": {
-        "N": 4832,
-        "S": 6786,
-        "E": 4737,
-        "W": 3612
-    },
-    "Anne": {
-        "N": 5239,
-        "S": 4802,
-        "E": 5820,
-        "W": 1859
-    },
-    "Fiona": {
-        "N": 3904,
-        "S": 3645,
-        "E": 8821,
-        "W": 2451
-    }
-}
-for x in d:
-    print(x)
-    for y in d[x]:
-        print(y, ": ", d[x][y])
-imya = input("Имя: ")
-region = input("Регион: ")
-print(d[imya][region])
-znach = input("Новое значение: ")
-d[imya][region] = znach
-print(d[imya])
+students = {}
+a = 0
+Kolvo_stud = int(input("Количество студентов: "))
+for i in range(1, Kolvo_stud + 1):
+    stud = input(str(i) + "-й студент: ")
+    ball = int(input("Балл: "))
+    students[stud] = ball
+    a += ball
+sredball = a/Kolvo_stud
+print(sredball)
+for i in students:
+    if students[i] > sredball:
+        print("Студенты с баллом выше среднего:", i)

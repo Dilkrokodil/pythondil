e9edf4b03623c578429bdf29eacd7a62b93ed41b
Dilkrokodil <dil.zubajdullina@yandex.ru>
2024-05-07 21:14:04 +0300

    dz

---
+++ geometry/rect.py
@@ -0,0 +1,21 @@
+class Rectangle:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def get_perimetr(self):
+        return 2 * (self.w + self.h)
+
+
+
+if __name__ == "__main__":
+    r1 = Rectangle(1, 2)
+    r2 = Rectangle(3, 4)
+    print(f"Perimetr prymoug: {r1.get_perimetr()}")
+
+
+# print(__name__)
+
+__author__ = "Admin"
+if __name__ == '__main__':
+     print(f"Module {__name__} (author: {__author__})")
